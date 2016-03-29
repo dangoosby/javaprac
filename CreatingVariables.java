db0e784879ad47939b856f37dcda0bf167a62d2d
macross <daniel@macross.io>
2016-03-28 23:34:47 -0400

    variables, sequencing, calculating exercises

--- CreatingVariables.java
+++ CreatingVariables.java
@@ -17,5 +17,10 @@ public class CreatingVariables {
         title = "Mr.";
 
         System.out.println( "The variable x contains " + x );
+        System.out.println( "The value " + y + " is stored in the variable y." );
+        System.out.println( "The experiment took " + seconds + " seconds." );
+        System.out.println( "A favorite irration # is Euler's number: " + e);
+        System.out.println( "Hopefully you have more than $" + checking + "!" );
+        System.out.println( "My name's " + title + " " + firstName + lastName );
     }
 }
